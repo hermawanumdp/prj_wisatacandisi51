265944848f033812222acd398d7c5da0c4744716
MDP SI51 (HN) <hermawan@mdp.ac.id>
2023-10-16 20:33:02 +0700

    Tampil Gambar
    Menampilkan gambar candi, akan tetapi belum menggunakan widget rectangle

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:prj_wisatacandisi51/widgets/detail_screen.dart';
+import 'package:prj_wisatacandisi51/data/candi_data.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,16 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DetailScreen(),
+      home: DetailScreen(candi: candiList[0]),
     );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    throw UnimplementedError();
   }
 }
---
+++ lib/widgets/detail_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:prj_wisatacandisi51/models/candi.dart';
+
+class DetailScreen extends StatelessWidget {
+  final Candi candi;
+
+  const DetailScreen({super.key, required this.candi});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: Image.asset(
+                  candi.imageAsset,
+                  width: double.infinity,
+                  height: 300,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
