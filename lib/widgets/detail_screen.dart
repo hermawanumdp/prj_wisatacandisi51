53311a1fa78a4e175929b760afd1445474740f87
MDP SI51 (HN) <126551695+hermawanumdp@users.noreply.github.com>
2023-10-21 18:38:23 +0700

    Info Candi
    Info atas informasi Candi

--- lib/widgets/detail_screen.dart
+++ lib/widgets/detail_screen.dart
@@ -15,14 +15,115 @@ class DetailScreen extends StatelessWidget {
             children: [
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 8),
-                child: Image.asset(
-                  candi.imageAsset,
-                  width: double.infinity,
-                  height: 300,
-                  fit: BoxFit.cover,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    candi.imageAsset,
+                    width: double.infinity,
+                    height: 300,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple.withOpacity(0.5),
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.arrow_back),
+                  ),
                 ),
               ),
             ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 8,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      candi.name,
+                      style: const TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.favorite_border),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.place,
+                      color: Colors.red,
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    const SizedBox(
+                      width: 70,
+                      child: Text(
+                        'Lokasi',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Text(' : ${candi.location}')
+                  ],
+                ),
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.calendar_month,
+                      color: Colors.blue,
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    const SizedBox(
+                      width: 70,
+                      child: Text(
+                        'Dibangun',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Text(' : ${candi.built}')
+                  ],
+                ),
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.house,
+                      color: Colors.green,
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    const SizedBox(
+                      width: 70,
+                      child: Text(
+                        'Tipe',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Text(' : ${candi.type}')
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
